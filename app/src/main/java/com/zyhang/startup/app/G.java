b73c14fea43cd149ba31a1ba6408ff1307be5659
EVIL MOUTH <izyhang@gmail.com>
2020-10-28 18:34:43 +0800

    publish to bintray

---
+++ app/src/main/java/com/zyhang/startup/app/G.java
@@ -0,0 +1,17 @@
+package com.zyhang.startup.app;
+
+import com.zyhang.startup.StartupTask;
+import com.zyhang.startup.StartupTaskRegister;
+import com.zyhang.startup.executor.IOExecutor;
+
+@StartupTaskRegister(id = "g.G", executorFactory = IOExecutor.Factory.class)
+public class G implements StartupTask {
+    @Override
+    public void startup() {
+        try {
+            Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
