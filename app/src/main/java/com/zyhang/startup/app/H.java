f68e24a7a00082e598c08c95fb783c58fd096fd3
EVIL MOUTH <izyhang@gmail.com>
2020-11-23 11:24:10 +0800

    update gradle

--- app/src/main/java/com/zyhang/startup/app/H.java
+++ app/src/main/java/com/zyhang/startup/app/H.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import com.zyhang.startup.AndroidStartupTask;
 import com.zyhang.startup.StartupTaskRegister;
 
+import org.jetbrains.annotations.NotNull;
+
 @StartupTaskRegister(
         id = "h.H",
         idDependencies = {"g.G"}
 )
 public class H extends AndroidStartupTask {
     @Override
-    public void startup(Context context) {
+    public void startup(@NotNull Context context) {
     }
 }
