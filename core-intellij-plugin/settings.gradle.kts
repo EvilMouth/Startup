adbc7906ca1cec7aae0ef0ea44a56d7ec19dcbe5
Evil Mouth <izyhang@gmail.com>
2021-10-18 10:59:13 +0800

    temporary remove intellij-plugin

--- core-intellij-plugin/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.4.21"
-    }
-}
-
-rootProject.name = "core-intellij-plugin"
-
--- core-intellij-plugin/src/main/kotlin/com/zyhang/startup/plugin/StartupCompatIssue.kt
+++
@@ -1,3 +0,0 @@
-package com.zyhang.startup.plugin
-
-class StartupCompatIssue(s: String = "oops, compat issue! please contact me.") : IllegalStateException(s)
